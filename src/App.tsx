43bb87c4a2eff0a969211b85955bf374992665be
Tiago Freitas <freitasts365@gmail.com>
2025-08-11 09:00:06 -0300

    Testando Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { Heading } from "./components/Heading";
 
 export function App() {
   return (
     <div>
-      <h1>Welcome to Chronos Pomodoro</h1>
+      <Heading />
       <p>Your productivity companion.</p>
     </div>
   );
