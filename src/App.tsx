f620a6c5b50f0154e29102591436d98ed16b20b6
Tiago Freitas <freitasts365@gmail.com>
2025-08-11 15:56:48 -0300

    CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <div>
-      <Heading />
+      <Heading>
+        Ola Mundo
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>Your productivity companion.</p>
     </div>
   );
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,11 @@
 import styles from "./Heading.module.css";
 
-export function Heading() {
-  return <h1 className={styles.Heading}>Welcome to Chronos Pomodoro</h1>;
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+/*Desestructuring the children prop to get the text content of the heading*/
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }
